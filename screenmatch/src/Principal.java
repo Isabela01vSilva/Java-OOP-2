6cf635689fb190eca31257e02890d4834da46b4d
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-08-06 11:06:05 -0300

    Herança

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -12,6 +13,16 @@ public class Principal {
         meuFilme.avalia(5);
         meuFilme.avalia(10);
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar: " + lost.getDuracaoEmMinutos());
     }
 }
